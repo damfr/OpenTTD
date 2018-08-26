fa6b7a4c5d284a03d06b430ddc448aa101417c97
ic111 <ic11@tt-forums>
2018-08-26 16:27:38 +0200

    TIP: Adjustments to the timetable window GUI

--- src/widgets/timetable_widget.h
+++ src/widgets/timetable_widget.h
@@ -14,21 +14,63 @@
 enum VehicleTimetableWidgets {
 	WID_VT_CAPTION,                     ///< Caption of the window.
 	WID_VT_ORDER_VIEW,                  ///< Order view.
-	WID_VT_TIMETABLE_PANEL,             ///< Timetable panel.
-	WID_VT_ARRIVAL_DEPARTURE_PANEL,     ///< Panel with the expected/scheduled arrivals.
-	WID_VT_SCROLLBAR,                   ///< Scrollbar for the panel.
+
+	WID_VT_SHIFT_BY_LENGTH_PAST_BUTTON, ///< Button for shifting the timetable into the past by its length
+	WID_VT_SHIFT_BY_LENGTH_FUTURE_BUTTON, ///< Button for shifting the timetable into the future by its length
+	WID_VT_FULL_FILTER_BUTTON,          ///< Filter button for showing all data lines
+	WID_VT_DESTINATION_FILTER_BUTTON,   ///< Filter button for showing the destination lines
+	WID_VT_TIMETABLE_FILTER_BUTTON,     ///< Filter button for showing the timetable lines
+
 	WID_VT_SUMMARY_PANEL,               ///< Summary panel.
-	WID_VT_START_DATE,                  ///< Start date button.
-	WID_VT_CHANGE_TIME,                 ///< Change time button.
-	WID_VT_CLEAR_TIME,                  ///< Clear time button.
-	WID_VT_RESET_LATENESS,              ///< Reset lateness button.
-	WID_VT_AUTOFILL,                    ///< Autofill button.
-	WID_VT_EXPECTED,                    ///< Toggle between expected and scheduled arrivals.
+	WID_VT_TIMETABLE_PANEL,             ///< Timetable panel.
+	WID_VT_SCROLLBAR,                   ///< Scrollbar for the panel.
+
+	WID_VT_TOP_SELECTION,               ///< Selection for the whole upper button row
+	WID_VT_SELECTION_TOP_1,             ///< Selection for the first button of the upper button row
+	WID_VT_NON_STOP_DROPDOWN,           ///< The non-stop drop down
+	WID_VT_NON_STOP_STATION_DROPDOWN,   ///< The non-stop drop down, for the station row
+	WID_VT_NON_STOP_WAYPOINT_DROPDOWN,  ///< The non-stop drop down, for the waypoint row
+	WID_VT_NON_STOP_DEPOT_DROPDOWN,     ///< The non-stop drop down, for the depot row
+	WID_VT_RENAME_BUTTON,               ///< Button for renaming a timetable
+	WID_VT_ARRIVAL_BUTTON,              ///< Button for setting the arrival of an order
+	WID_VT_START_BUTTON,                ///< The button for setting the start of the timetable
+	WID_VT_AUTOFILL_SELECTION,          ///< Selection for the autofill start vs. stop buttons
+	WID_VT_START_AUTOFILL_DROPDOWN,     ///< The dropdown for starting autofill.
+	WID_VT_STOP_AUTOFILL_BUTTON,        ///< The button for stopping autofill.
+	WID_VT_AUTOFILL_INFO_PANEL,         ///< Panel with status information about autofill
+	WID_VT_COND_VARIABLE_DROPDOWN,      ///< Dropdown for the variable of conditional orders
+
+	WID_VT_SELECTION_TOP_2,             ///< Selection for the second button of the upper button row
+	WID_VT_FULL_LOAD_DROPDOWN,          ///< The full load drop down
+	WID_VT_REFIT_BUTTON,                ///< The refit button
+	WID_VT_OFFSET_BUTTON,               ///< The button for setting the offset of the timetable
+	WID_VT_COND_COMPARATOR_DROPDOWN,    ///< Dropdown for the comparator of conditional orders
+
+	WID_VT_SELECTION_TOP_3,             ///< Selection for the third button of the upper button row
+	WID_VT_UNLOAD_DROPDOWN,             ///< The unload drop down
+	WID_VT_SERVICE_DROPDOWN,            ///< The service dropdown
+	WID_VT_SPEEDLIMIT_BUTTON,           ///< The button for setting the speed limit
+	WID_VT_LENGTH_BUTTON,               ///< The button for setting the length of the timetable
+	WID_VT_COND_VALUE_BUTTON,           ///< Button for the value of conditional orders
+
+	WID_VT_SELECTION_TOP_4,             ///< Selection for the fourth button of the upper button row
+	WID_VT_REFIT_SELECTION,             ///< Selection for refit button vs. dropdown
+	WID_VT_REFIT_BUTTON_4,              ///< The refit button
+	WID_VT_REFIT_AUTO_DROPDOWN,         ///< Drop down for auto refit
+	WID_VT_DEPARTURE_BUTTON,            ///< The button for setting the departure time of an order
+	WID_VT_SHIFT_ORDERS_PAST_BUTTON,    ///< Button for shifting parts of the timetable into the past by a specified amount
+	WID_VT_SHIFT_ORDERS_FUTURE_BUTTON,  ///< Button for shifting parts of the timetable into the future by a specified amount
+
 	WID_VT_SHARED_ORDER_LIST,           ///< Show the shared order list.
-	WID_VT_ARRIVAL_DEPARTURE_SELECTION, ///< Disable/hide the arrival departure panel.
 	WID_VT_EXPECTED_SELECTION,          ///< Disable/hide the expected selection button.
-	WID_VT_CHANGE_SPEED,                ///< Change speed limit button.
-	WID_VT_CLEAR_SPEED,                 ///< Clear speed limit button.
+
+	WID_VT_SKIP_ORDER_BUTTON,           ///< The skip order button
+
+	WID_VT_SELECTION_BOTTOM_2,          ///< Selection for the second button in the lower button row
+	WID_VT_DELETE_ORDER_BUTTON,         ///< Button for deleting an order
+	WID_VT_STOP_SHARING_BUTTON,         ///< Button for stopping sharing orders
+
+	WID_VT_GOTO_BUTTON,                 ///< The goto button
 };
 
 #endif /* WIDGETS_TIMETABLE_WIDGET_H */
