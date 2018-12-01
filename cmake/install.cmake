c31abdc5ea8bd941ca48d315100c923e589832d4
damfr <damfr@users.noreply.github.com>
2018-12-01 11:18:47 +0100

    Add: [CMake] Global data dir installation variable

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,1 +1,4 @@
 #This file sets various variables related to installation of openttd
+#Global data dir
+set(GLOBAL_DATA_DIR ${CMAKE_INSTALL_PREFIX}/share/games/openttd CACHE STRING "Global data dir")
+target_compile_definitions(openttd PRIVATE GLOBAL_DATA_DIR=\"${GLOBAL_DATA_DIR}\")
